aaa4908d21eea7159a469259ddc90f15bc64c617
stiffni <steffaniefreeman@gmail.com>
2024-05-26 22:48:34 +0200

    style(card-css): update css for Card component

--- app/components/atoms/Card/index.tsx
+++ app/components/atoms/Card/index.tsx
@@ -1,5 +1,7 @@
 export function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="rounded-sm border-8 border-yellow-500">{children}</div>
+    <div className="text-slate-900 rounded-sm border-8 border-yellow-400 p-4 lg:p-16 bg-gradient-to-br from-yellow-300 from-25% via-amber-400 via-40% to-yellow-300 to-90% shadow-inner min-w-full lg:min-w-auto ">
+      {children}
+    </div>
   );
 }
