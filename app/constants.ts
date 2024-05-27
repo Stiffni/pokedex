820bdea66e1e32fa338060219cafda4430ae3a89
stiffni <steffaniefreeman@gmail.com>
2024-05-27 07:14:08 +0200

    refactor(defaults): move placeholder and error pokemon to constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,27 @@
+import { Pokemon } from "./types";
+
 export const MAX_POKEMON = 1025;
+
+export const PLACEHOLDER_POKEMON: Pokemon = {
+  name: "Loading...",
+  sprites: {
+    other: { "official-artwork": { front_default: "/pokemon-loading.gif" } },
+  },
+  moves: [{ move: { name: "" } }],
+  types: [{ type: { name: "" } }],
+};
+
+export const ERROR_POKEMON: Pokemon = {
+  name: "Surprised Pikachu",
+  sprites: {
+    other: { "official-artwork": { front_default: "/surprise-pikachu.webp" } },
+  },
+  moves: [{ move: { name: "" } }],
+  types: [
+    {
+      type: {
+        name: "Error - Verify your search term is correct or check your internet connection",
+      },
+    },
+  ],
+};
