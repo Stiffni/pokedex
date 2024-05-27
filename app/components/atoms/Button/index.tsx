0aa46664ac32dc1c9b1eeabd160085d0d5298793
stiffni <steffaniefreeman@gmail.com>
2024-05-27 07:13:27 +0200

    style(button): update hover/active/disabled button styles

--- app/components/atoms/Button/index.tsx
+++ app/components/atoms/Button/index.tsx
@@ -6,7 +6,7 @@ export function Button({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className="min-w-10 flex justify-center items-center border-2 border-white rounded-sm hover:bg-yellow-500 hover:text-slate-900"
+      className="min-w-10 flex justify-center items-center border-2 border-white rounded-sm hover:bg-yellow-500 hover:text-slate-800 disabled:bg-slate-200 disabled:text-slate-800 active:bg-amber-500"
       {...props}
     >
       {children}
